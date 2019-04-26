7c6b506b6531d32225077d72c73607ed2ad14e36
mihai <mihai@uniplaces.com>
2019-04-26 16:35:19 +0100

    Update JS projects

--- learn js/powerFunction.js
+++ learn js/powerFunction.js
@@ -1,19 +1,31 @@
 //function that calculates the power of a number (base and exponent)
 const power = function (base, exponent) {
     let result = 1;
-    for (let count = 0; count <= exponent; count++) {
+    for (let count = 0; count < exponent; count++) {
         result *= base;
     }
-    console.log(result);
+    return result;
 }
-power(9, 3);
+console.log(power(9, 3));
+
 
 //arrow function
 const power = (base, exponent) => {
     let result = 1;
-    for (let count = 0; count <= exponent; count++) {
+    for (let count = 0; count < exponent; count++) {
         result *= base;
     }
     return result;
 };
+console.log(power(9, 3));
+
+
+//power function by using recursive function
+const power = (base, exponent) => {
+    if (exponent == 0) {
+        return 1;
+    } else {
+        return base * power(base, exponent - 1)
+    }
+}
 console.log(power(9, 3));
